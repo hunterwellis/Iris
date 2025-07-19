e2837fd7c75cdf3c89ac70f12af1abc3bff6076b
Hunter Ellis <elliswhunter@gmail.com>
2025-07-19 12:58:10 -0400

    refactored project strucuture

---
+++ apps/dashboard/ui/main_window.cpp
@@ -0,0 +1,27 @@
+#include <QLayout>
+#include <QtWidgets>
+
+#include "main_window.hpp"
+#include "widgets/habit_tracker.hpp"
+
+typedef int row, col, rowspan, colspan;
+
+MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
+
+  // lg = new logoWidget(this);
+  // dc = new digitalClock;
+  ac = new analogClock;
+  // ht = new habitTracker("Habit Title", this);
+
+  QGridLayout *layout = new QGridLayout;
+  // layout->addWidget(lg, row{0}, col{0}, rowspan{1}, colspan{2},
+  // Qt::AlignLeft);
+  // layout->addWidget(dc, row{0}, col{0}, Qt::AlignBottom);
+  layout->addWidget(ac, row{0}, col{0}, Qt::AlignTop);
+  // layout->addWidget(ht, 1, 0, Qt::AlignCenter);
+
+  setStyleSheet("background-color: #000000;"); // black background to keep
+                                               // mirror reflective
+  setLayout(layout);
+  setWindowTitle("Iris");
+}
--- apps/dashboard/ui/main_window.hpp
+++ apps/dashboard/ui/main_window.hpp
@@ -1,24 +1,24 @@
 #ifndef MAIN_WINDOW_HPP
 #define MAIN_WINDOW_HPP
 
-#include <QWidget>
 #include <QCalendarWidget>
+#include <QWidget>
 
-#include "widgets/habit_tracker.hpp"
-#include "widgets/logo.hpp"
 #include "widgets/analog_clock.hpp"
 #include "widgets/digital_clock.hpp"
+#include "widgets/habit_tracker.hpp"
+#include "widgets/logo.hpp"
 
-class MainWindow: public QWidget{
+class MainWindow : public QWidget {
   Q_OBJECT
 
 public:
   MainWindow(QWidget *parent = nullptr);
 
 private:
-  logoWidget *logo;
-  analogClock *ac;
+  logoWidget *lg;
   digitalClock *dc;
+  analogClock *ac;
   habitTracker *ht;
 };
 
